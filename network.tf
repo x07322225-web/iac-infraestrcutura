37335d0f95e10b28c923120dbd80ffdc42760ee9
x07322225-web <x07322225@gmail.com>
2026-03-08 20:26:39 -0600

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "red_principal" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "red-iac"
+  }
+}
+
+resource "aws_subnet" "subred" {
+  vpc_id     = aws_vpc.red_principal.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subred-iac"
+  }
+}
